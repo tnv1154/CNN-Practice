cded27af8b388af165ebedafd9fa35019779aa7e
tnv1154 <ngvietthang.attt.ptit@gmail.com>
2025-04-04 23:33:29 +0700

    04/04/2025

--- Convolution.py
+++ Convolution.py
@@ -1,13 +1,25 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 img =cv2.imread("Thang_1.jpg")
 img = cv2.resize(img, (500, 500))
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-print(img_gray.shape)
+#print(img_gray.shape)
 
 def conv2d(input, kernelSize):
     height, width = input.shape
     kernel = np.random.randn(kernelSize, kernelSize)
-    print(kernel)
+    #print(kernel)
+    result = np.zeros((height - kernelSize + 1, width + kernelSize + 1))
+
+    for row in range(0, height - kernelSize + 1):
+        for col in range(0, width - kernelSize + 1):
+            result[row, col] = np.sum(input[row : row + kernelSize, col : col + kernelSize] * kernel)
+    return result
+
+img_gray_cov2d = conv2d(img_gray, 3)
+
+plt.imshow(img_gray_cov2d, cmap='gray')
+plt.show()
